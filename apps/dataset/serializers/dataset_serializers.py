e3acec044d0d2d03c44a51d44d6705906f446bdc
又菜又爱玩的小朱猪 <107445049+yxsj245@users.noreply.github.com>
2025-05-19 09:38:10 +0800

    Update dataset_serializers.py

--- apps/dataset/serializers/dataset_serializers.py
+++ apps/dataset/serializers/dataset_serializers.py
@@ -411,7 +411,7 @@ class DataSetSerializers(serializers.ModelSerializer):
                                 'embedding_mode_id': instance.get('embedding_mode_id')}
             return self.save(dataset_instance, with_valid=True)
 
-        @valid_license(model=DataSet, count=50,
+        @valid_license(model=DataSet, count=999,
                        message=_(
                            'The community version supports up to 50 knowledge bases. If you need more knowledge bases, please contact us (https://fit2cloud.com/).'))
         @post(post_function=post_embedding_dataset)
