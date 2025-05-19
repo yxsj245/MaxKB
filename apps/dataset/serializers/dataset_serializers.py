9977f0297c9f5801df43991488ed02038cf9eb40
又菜又爱玩的小朱猪 <107445049+yxsj245@users.noreply.github.com>
2025-05-19 10:49:08 +0800

    Add files via upload

--- apps/dataset/serializers/dataset_serializers.py
+++ apps/dataset/serializers/dataset_serializers.py
@@ -54,20 +54,20 @@ from smartdoc.conf import PROJECT_DIR
 from django.utils.translation import gettext_lazy as _
 
 """
-# __exact  精确等于 like ‘aaa’
+# __exact  精确等于 like 'aaa'
 # __iexact 精确等于 忽略大小写 ilike 'aaa'
 # __contains 包含like '%aaa%'
-# __icontains 包含 忽略大小写 ilike ‘%aaa%’，但是对于sqlite来说，contains的作用效果等同于icontains。
+# __icontains 包含 忽略大小写 ilike '%aaa%',但是对于sqlite来说，contains的作用效果等同于icontains。
 # __gt  大于
 # __gte 大于等于
 # __lt 小于
 # __lte 小于等于
 # __in 存在于一个list范围内
-# __startswith 以…开头
-# __istartswith 以…开头 忽略大小写
-# __endswith 以…结尾
-# __iendswith 以…结尾，忽略大小写
-# __range 在…范围内
+# __startswith 以...开头
+# __istartswith 以...开头 忽略大小写
+# __endswith 以...结尾
+# __iendswith 以...结尾，忽略大小写
+# __range 在...范围内
 # __year 日期字段的年份
 # __month 日期字段的月份
 # __day 日期字段的日
@@ -411,7 +411,7 @@ class DataSetSerializers(serializers.ModelSerializer):
                                 'embedding_mode_id': instance.get('embedding_mode_id')}
             return self.save(dataset_instance, with_valid=True)
 
-        @valid_license(model=DataSet, count=999,
+        @valid_license(model=DataSet, count=999999999,
                        message=_(
                            'The community version supports up to 50 knowledge bases. If you need more knowledge bases, please contact us (https://fit2cloud.com/).'))
         @post(post_function=post_embedding_dataset)
